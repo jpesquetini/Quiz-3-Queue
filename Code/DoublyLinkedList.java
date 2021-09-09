01b8febb1e7658972c14ed6eb6d5d80ea4e611ca
Joae Pablo Esquetini <jpesquetinif@gmail.com>
2021-09-08 21:06:05 -0600

    Cambios minimos

--- Code/DoublyLinkedList.java
+++ Code/DoublyLinkedList.java
@@ -51,47 +51,4 @@ public class DoublyLinkedList {
             return temp;
         }
     }
-
-    /*
-    public static void main(String[] args) {
-        DoublyLinkedList prueba = new DoublyLinkedList();
-        
-        prueba.insertLast(23);
-        prueba.insertLast(24);
-        prueba.insertLast(25);
-        prueba.insertLast(26);
-        prueba.insertLast(27);
-
-        int size = prueba.getSize();
-        System.out.println(size);
-
-        Object data1 = prueba.head.getData();
-        Object data2 = prueba.head.next.getData();
-        Object data3 = prueba.head.next.next.getData();
-        Object data4 = prueba.head.next.next.next.getData();
-        Object data5 = prueba.head.next.next.next.next.getData();
-
-        System.out.println(data1);
-        System.out.println(data2);
-        System.out.println(data3);
-        System.out.println(data4);
-        System.out.println(data5);
-
-        prueba.deleteFirst();
-        prueba.deleteFirst();
-        prueba.deleteFirst();
-
-        int size2 = prueba.getSize();
-        System.out.println(size2);
-
-        Object data6 = prueba.head.getData();
-        Object data7 = prueba.head.next.getData();
-
-        System.out.println(data6);
-        System.out.println(data7);
-
-        Node dequeued = prueba.deleteFirst();
-        System.out.println(dequeued.getData());
-    }
-    */
 }
